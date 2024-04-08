2c2552db0b62a330c29077d7d8cd97d90cd006c8
simararustam <simararustam@gmail.com>
2024-04-08 13:14:23 +0000

    Write a function that creates a binary tree node

---
+++ binary_trees.h
@@ -0,0 +1,24 @@
+#ifndef HH
+#define HH
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ *
+ * Description: Basic Binary Tree
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;i
+    struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+#endif
