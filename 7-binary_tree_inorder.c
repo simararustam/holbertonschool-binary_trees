eea5ecd9697ed2891843d1edcea8bb9af8014142
samiribrh <samiribrahimov2277@gmail.com>
2024-04-09 10:43:27 +0400

    Func name changed

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -14,7 +14,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	temp = tree;
 	if (temp == NULL || func == NULL)
 		return;
-	binary_tree_preorder(temp->left, func);
+	binary_tree_inorder(temp->left, func);
 	func(temp->n);
-	binary_tree_preorder(temp->right, func);
+	binary_tree_inorder(temp->right, func);
 }
