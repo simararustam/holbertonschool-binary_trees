f718183aa1d53d837198244eec61a48c66dbc91d
simararustam <simararustam@gmail.com>
2024-04-09 20:54:22 +0000

    betty

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 /**
- *
- *
+ *binary_tree_is_full - checks if a binary tree is full
+ *@tree: the root node of the tree
+ *Return: 1 or 0
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
