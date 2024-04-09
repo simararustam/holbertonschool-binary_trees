a4bdd1e2170eb7ceb79c7396cfb5adf9d6c98c50
samiribrh <samiribrahimov2277@gmail.com>
2024-04-09 11:08:33 +0400

    Condition change

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left, right;
 
-	if (tree == NULL || tree->left == NULL || tree->right == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
