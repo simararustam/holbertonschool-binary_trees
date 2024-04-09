c519490e53a017366a5298460a0ec582a0ba0821
samiribrh <samiribrahimov2277@gmail.com>
2024-04-09 11:04:46 +0400

    Unnecessary variable removed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,15 +8,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height, left, right;
+	size_t left, right;
 
 	if (tree == NULL || tree->left == NULL || tree->right == NULL)
 		return (0);
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 	if (left > right)
-		height = left + 1;
+		return (left + 1);
 	else
-		height = right + 1;
-	return (height);
+		return (right + 1);
 }
