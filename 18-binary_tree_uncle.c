ec245f15c85218c9f0596e613a62726b1e15f985
samiribrh <samiribrahimov2277@gmail.com>
2024-04-10 00:22:44 +0400

    Node uncle find func

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Finds the uncle of binary tree.
+ * @node: The binary tree
+ *
+ * Return: Pointer to the uncle node,
+ *	NULL if fails.
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *temp;
+
+	if (node == NULL)
+		return (NULL);
+	temp = node->parent->parent;
+	if (temp->left == node->parent)
+		return (temp->right);
+	return (temp->left);
+}
